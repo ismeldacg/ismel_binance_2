f46afeaf49aafdb27d794313fa1e75506556293b
Luis Ramos <luisenriqueramos1977@gmail.com>
2021-08-07 08:23:40 +0200

    status 8:30

--- binance_test1.py
+++ binance_test1.py
@@ -513,6 +513,21 @@ while not(keyboard.is_pressed('q')):
                         if cummulativeQuantity==0:
                             cummulativeQuantity=20
 
+                        #increasing the trading price for a symbol******************
+                        #granting we can purchase more of one specific coin
+                        if "XRPUSDT" in aSymbol and cummulativeQuantity < 60:
+                            print('increasing '+aSymbol+' purchase amount to 60')
+                            cummulativeQuantity=60
+                        elif "BTTUSDT" in aSymbol and cummulativeQuantity < 40:
+                            print('increasing '+aSymbol+' purchase amount to 40')
+                            cummulativeQuantity=40
+                        elif "TRXUSDT" in aSymbol and cummulativeQuantity < 30:
+                            print('increasing '+aSymbol+' purchase amount to 30')
+                            cummulativeQuantity=30
+                        elif "SHIBUSDT" in aSymbol and cummulativeQuantity < 30:
+                                print('increasing '+aSymbol+' purchase amount to 30')
+                                cummulativeQuantity=30
+
                         
                         
                         current_str_symbol_price=symbol_price["price"]
