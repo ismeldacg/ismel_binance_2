b8ab9f217797f62aee3aed786280965698946f77
Luis Ramos <luisenriqueramos1977@gmail.com>
2021-08-07 09:11:31 +0200

    status 9:11

--- binance_test1.py
+++ binance_test1.py
@@ -515,18 +515,18 @@ while not(keyboard.is_pressed('q')):
 
                         #increasing the trading price for a symbol******************
                         #granting we can purchase more of one specific coin
-                        if "XRPUSDT" in aSymbol and cummulativeQuantity < 60:
-                            print('increasing '+aSymbol+' purchase amount to 60')
-                            cummulativeQuantity=60
-                        elif "BTTUSDT" in aSymbol and cummulativeQuantity < 40:
-                            print('increasing '+aSymbol+' purchase amount to 40')
-                            cummulativeQuantity=40
-                        elif "TRXUSDT" in aSymbol and cummulativeQuantity < 30:
-                            print('increasing '+aSymbol+' purchase amount to 30')
-                            cummulativeQuantity=30
-                        elif "SHIBUSDT" in aSymbol and cummulativeQuantity < 30:
-                                print('increasing '+aSymbol+' purchase amount to 30')
-                                cummulativeQuantity=30
+                        # if "XRPUSDT" in aSymbol and cummulativeQuantity < 60:
+                        #     print('increasing '+aSymbol+' purchase amount to 60')
+                        #     cummulativeQuantity=60
+                        # elif "BTTUSDT" in aSymbol and cummulativeQuantity < 40:
+                        #     print('increasing '+aSymbol+' purchase amount to 40')
+                        #     cummulativeQuantity=40
+                        # elif "TRXUSDT" in aSymbol and cummulativeQuantity < 30:
+                        #     print('increasing '+aSymbol+' purchase amount to 30')
+                        #     cummulativeQuantity=30
+                        # elif "SHIBUSDT" in aSymbol and cummulativeQuantity < 30:
+                        #         print('increasing '+aSymbol+' purchase amount to 30')
+                        #         cummulativeQuantity=30
 
                         
                         
